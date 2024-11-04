272c015dfc033aad200f124667ddd5db09f76ec1
Shruti <shruticg2013@gmail.com>
2024-11-04 21:29:01 +0530

    dashes

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-let letters; 
-let wordLoaded = false; // Variable to track if the word is already loaded
-// edge case , the letters can be repeated 
+let letters;
+let wordLoaded = false; 
+let attempts = 6; // Number of incorrect attempts allowed
+let guessedLetters = []; 
+let correctLetters = []; 
 
-$(document).ready(function(){
+$(document).ready(function() {
     getWord();
    
-    $(".button").click(function(){
+    $(".button").click(function() {
         var value = $(this).val();
         console.log(value);
-        fill(letters,value);
+        fill(letters, value);
         
-    $(this).prop("disabled", true);
-$(this).addClass("disable");
+        $(this).prop("disabled", true);
+        $(this).addClass("disable");
     });
 });
 
@@ -22,45 +24,80 @@ function getWord() {
         })
         .then(response => response.json())
         .then(data => {
-            const Word = data[0];
-            console.log('Random word:', Word);
-            letters = Word.split('');
+            const word = data[0];
+            console.log('Random word:', word);
+            letters = word.split('');   
             console.log(letters);
             wordLoaded = true; 
             dashes(letters);
-         
             return letters; 
         })
         .catch(error => console.error('Error fetching random word:', error));
     } else {
-        console.log('Word already loaded:', letters); // If word is already loaded, log it
+        console.log('Word already loaded:', letters); 
         return Promise.resolve(letters); 
     }
 }
-function dashes(letters){
 
-    let dashedword =''
-    for (let i =0;i<letters.length;i++){
-        dashedword += "<span class='dash'>-</span>";
+function dashes(letters) {
+    let dashedWord = '';
+    for (let i = 0; i < letters.length; i++) {
+        dashedWord += "<span class='dash'>-</span>";
     }
-    document.getElementById("container").innerText = dashedword;
+    document.getElementById("container").innerHTML = dashedWord;
+    updateAttempts();
 }
+
 function fill(letters, value) {
     value = value.toLowerCase();
-    let dashedWord = document.getElementById("container").innerText.toLowerCase();
-    let newDashedWord = '';
+    if (guessedLetters.includes(value)) {
+        console.log("Already guessed: " + value);
+        return; 
+    }
+    guessedLetters.push(value);
+    let correctGuess = false;
 
+    let dashedWord = '';
     for (let i = 0; i < letters.length; i++) {
         if (letters[i].toLowerCase() === value) {
-            newDashedWord += "<span class='dash'>" + value +"</span>";
+            dashedWord += "<span class='dash'>" + value + "</span>";
+            correctGuess = true; // Found a correct guess
+            correctLetters[i] = value; // Store the correct letter
         } else {
-            newDashedWord +="<span class='dash'>-</span>";
+            dashedWord += "<span class='dash'>" + (correctLetters[i] || '-') + "</span>";
         }
     }
-    document.getElementById("container").innerText = newDashedWord;
+    
+    document.getElementById("container").innerHTML = dashedWord;
+
+    if (!correctGuess) {
+        attempts--; // Decrease attempts if guess was wrong
+        updateAttempts();
+    }
+    
+    checkGameStatus();
 }
 
+function updateAttempts() {
+    document.getElementById("attempts").innerText = "Attempts left: " + attempts;
+}
 
+function checkGameStatus() {
+    if (attempts <= 0) {
+        alert("Game Over! The word was: " + letters.join(''));
+        resetGame();
+    } else if (correctLetters.length && correctLetters.length === letters.length && !correctLetters.includes(undefined)) {
+        alert("Congratulations! You've guessed the word: " + letters.join(''));
+        resetGame();
+    }
+}
 
-// function hang(){
-// }
+function resetGame() {
+    letters = null;
+    wordLoaded = false;
+    attempts = 6;
+    guessedLetters = [];
+    correctLetters = [];
+    $(".button").prop("disabled", false).removeClass("disable");
+    getWord();
+}
